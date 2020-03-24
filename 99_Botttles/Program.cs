1118d73b30d6c53758f1c68e4340a867478fb76b
stevenshepherd823 <SandE66458112>
2020-03-23 17:36:17 -0700

    Initial check-in of module 99_Botttles

---
+++ 99_Botttles/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _99_Botttles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How many bottles are on the wall?");
+            int bottles = Convert.ToInt32(Console.ReadLine());
+
+            while (bottles > 1)
+            {
+                Console.WriteLine($"{bottles} bottles of beer on the wall.");
+                Console.WriteLine($"{bottles} bottles of beer!");
+                Console.WriteLine($"Take one down, Pass it around");
+                bottles--;
+                Console.WriteLine($"{bottles} of beer on the wall!");
+                Console.WriteLine("");
+            }
+                Console.WriteLine($"Last bottle of beer on the wall.");
+                Console.WriteLine($"Last bottle of beer!");
+                Console.WriteLine($"Take it down, Pass it around");
+               
+                Console.WriteLine($"No MORE Bootles of beer on the wall!");
+
+            
+            
+        }
+    }
+}
